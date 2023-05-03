a4a32947b0a95f599bc453b3c11881dc38337474
fajarm <andhikadika7878@gmail.com>
2023-05-03 14:54:45 +0700

    push deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send('Main!');
+	res.send('Deployment!');
 });
 
 const PORT = process.env.PORT || 8080;
