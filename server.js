f1c8ae54f1259fcb86279cf32d24fafd9b096e75
fajarm <andhikadika7878@gmail.com>
2023-05-03 16:25:20 +0700

    push "Update Production"

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send('Production!');
+	res.send('Update Production!');
 });
 
 const PORT = process.env.PORT || 8080;
